35f7d86341d46355c0c46a0469d789ad8d132a17
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-02-21 05:25:50 +0800

    Add files via upload

---
+++ AccountLoginSimulation/AccountLoginSimulation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLoginSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Account Login Simulation --
+            Console.WriteLine("(Account Login Simulation)");
+
+            string[] usernames = { "admin", "Anna", "Sussy" };
+            string[] passwords = { "admin123", "hana", "baka" };
+
+            Console.Write("Enter Username: ");
+            var inputUsername = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            var inputPassword = Console.ReadLine();
+
+            bool isSuccessful = false;
+
+            for (var i = 0; i < usernames.Length; i++)
+            {
+                if (usernames[i].Equals(inputUsername) && passwords[i].Equals(inputPassword))
+                {
+                    isSuccessful = true;
+                    break;
+                }
+            }
+
+            if (isSuccessful)
+            {
+                Console.WriteLine($"Welcome, {inputUsername}!");
+            }
+            else
+            {
+                Console.WriteLine("Account Not Found!");
+            }
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
