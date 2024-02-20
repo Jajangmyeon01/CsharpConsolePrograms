f9b5b35c5e2b0f76e1f109f92874e05d8ac1aed4
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-02-21 05:27:10 +0800

    Add files via upload

---
+++ AuthenticationSimulationV2/AuthenticationSimulationV2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationSimulationV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Authentication SimulationV2 --
+            Console.WriteLine("(Authentication SimulationV2)");
+
+            string[] userName = { "BankLogin", "Sterling", "ebay44" };
+            string[] passWord = { "BankLogin13", "SterlingGmal2024", "ebay.44" };
+
+            Console.Write("Enter Username : ");
+            var inputUserIndex = Console.ReadLine();
+            Console.Write("Enter Password : ");
+            var inputPasswordIndex = Console.ReadLine();
+            bool isLogin = false;
+
+            for (int user = 0; user < userName.Length; user++)
+            {
+                if (inputUserIndex.Equals(userName[user]) && inputPasswordIndex.Equals(passWord[user]))
+                //, StringComparison.InvariantCultureIgnoreCase 
+                {
+                    isLogin = true;
+                    Console.WriteLine($"Welcome, {userName[user]}!");
+                    break;
+                }
+
+            }
+            if (!isLogin)
+            {
+                Console.WriteLine("Account Not Found!");
+            }
+
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
