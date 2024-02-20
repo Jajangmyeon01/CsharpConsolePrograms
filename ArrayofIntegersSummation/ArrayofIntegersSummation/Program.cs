eea76c3b8ba1e4c0d4fd90159424cd536b526748
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-02-21 05:26:06 +0800

    Add files via upload

---
+++ ArrayofIntegersSummation/ArrayofIntegersSummation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayofIntegersSummation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Array of Integers Summation --
+            Console.WriteLine("(Array of Integers Summation)");
+
+            int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            int total = 0;
+            foreach (var num in numbers)
+            {
+                total += num;
+            }
+            Console.WriteLine($"Total Sum : {total}");
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
