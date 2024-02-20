0f47cc35875f3e26d2a3ebcf861d70b98d60523b
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-02-21 05:28:46 +0800

    Add files via upload

---
+++ NameDisplayusingForLoop/NameDisplayusingForLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameDisplayusingForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Name Display using For Loop
+            Console.WriteLine("(Name Display using For Loop)");
+
+            string[] arrayNames = new string[3];
+
+            for (int b = 0; b < 3; b++)
+            {
+                Console.Write($"Enter Name {b + 1} : ");
+                arrayNames[b] = Console.ReadLine();
+            }
+
+
+            for (int index = 0; index < arrayNames.Length; index++)
+            {
+                Console.WriteLine(arrayNames[index]);
+            }
+
+            #endregion
+
+            Console.ReadLine();
+
+        }
+    }
+}
