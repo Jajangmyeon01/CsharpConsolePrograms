f5986aa9e9723469b4747eb80439edd4f778b263
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-13 00:07:11 +0800

    Add files via upload

---
+++ UserInputObjectCreation/UserInputObjectCreation/CarsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputObjectCreation
+{
+    internal class CarsModel
+    {
+        public string carName;
+        public string Color;
+        public string Description;
+        public float price;
+
+        public CarsModel(string carName, string Color, string Description, float price)
+        {
+            this.carName = carName;
+            this.Color = Color;
+            this.Description = Description;
+            this.price = price;
+
+        }
+    }
+}
---
+++ UserInputObjectCreation/UserInputObjectCreation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputObjectCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region -- UserInputObjectCreation --
+            Console.Write("Enter CarName : ");
+            var carName = Console.ReadLine();
+
+            Console.Write("Enter Color : "); ;
+            var color = Console.ReadLine();
+
+            Console.Write("Enter Description : ");
+            var description = Console.ReadLine();
+
+            Console.Write("Enter Price : ");
+            var price = Convert.ToInt64(Console.ReadLine());
+
+            CarsModel car = new CarsModel(carName, color, description, price);
+
+            Console.WriteLine($"CarName : {car.carName}\n Color : {car.Color}\n Description : {car.Description}\n Price : {car.price}");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
