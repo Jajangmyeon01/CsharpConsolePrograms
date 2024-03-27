6160451f17c9f054f219034a3202a5b50347bc1b
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-27 09:23:19 +0800

    Add files via upload

---
+++ Student Registration Simulation/Student Registration Simulation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Registration_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Student Registration Simulation --
+            Console.WriteLine("(Studen Registration Simulation)");
+
+            Console.Write("Student Count : ");
+            var count = Convert.ToInt32(Console.ReadLine());
+
+            Student[] students = new Student[count];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Student : " + (i + 1));
+                Console.Write("Enter FirstName: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Enter LastName: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Enter Year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Course: ");
+                string course = Console.ReadLine();
+
+                Console.Write("Enter Section : ");
+                char section = Convert.ToChar(Console.ReadLine());
+
+                students[i] = new Student(firstName, lastName, year, course, section);
+
+                
+            }
+            foreach(Student student in students)
+            {
+                student.introduceSelf();
+            }
+             
+            
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student Registration Simulation/Student Registration Simulation/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Registration_Simulation
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int year { get; set; }
+        public string course { get; set; }
+        public char section { get; set; }
+
+        public Student(string firstName, string lastName, int year, string course, char section)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.year = year;
+            this.course = course;
+            this.section = section;
+
+        }
+        public void introduceSelf()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Full Name : {lastName} {firstName}");
+            Console.WriteLine($"Course/Year/Section : {course} {year} {section}");
+        }
+
+    }
+}
